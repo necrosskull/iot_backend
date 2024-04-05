3c722f6bc012f21d39ffb1a8a10b8427cf80a937
Slava Kuryaev <92830706+necrosskull@users.noreply.github.com>
2024-04-05 14:01:12 +0300

    update: add arduino endpoint

--- app/main.py
+++ app/main.py
@@ -20,13 +20,23 @@ class LampStatus(Enum):
 class DefaultLamps(Enum):
     lamp1 = "lamp1"
     lamp2 = "lamp2"
-    # lamp3 = "lamp3"
-    # lamp4 = "lamp4"
+    lamp3 = "lamp3"
+    lamp4 = "lamp4"
 
 
 class Lamp(BaseModel):
     name: DefaultLamps
     status: LampStatus
+
+
+class ArduinoStatus(Enum):
+    on = "1"
+    off = "0"
+
+
+class ArduinoLamp(BaseModel):
+    d: str
+    s: int
 
 
 @asynccontextmanager
@@ -55,6 +65,20 @@ async def read_lamps():
         lamps.append(
             Lamp(name=lamp, status=LampStatus(await redis.get(f"lamps:{lamp}")))
         )
+    return lamps
+
+
+@app.get("/ard", response_model=list[ArduinoLamp])
+async def read_arduino_lamps():
+    lamps = []
+    for lamp in DefaultLamps.__members__.values():
+        lamp_num = lamp.name.split("lamp")[1]
+        lamp_status = await redis.get(f"lamps:{lamp}")
+        if lamp_status == "on":
+            lamp_status = "1"
+        else:
+            lamp_status = "0"
+        lamps.append(ArduinoLamp(d=lamp_num, s=lamp_status))
     return lamps
 
 
